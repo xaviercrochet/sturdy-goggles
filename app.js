7c1b4dc5217eb8396ccade60e6106e865a1aad42
Xavier Crochet <xacrochet@gmail.com>
2016-09-05 11:50:16 +0200

    Added basic menu navigation

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-$('#menu').click(function(e){
-	console.log("coucou")
+$(function(){
+	var menuOpened = false;
+	$('#menu-button').click(function(e){
+		if(menuOpened) {
+			$('.menu').css("transform", "translate(0%)");	
+		}
+		else {
+			$('.menu').css("transform", "translate(100%)");	
+		}
+		menuOpened = !menuOpened & true;
+	});
 });
